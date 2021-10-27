c375f57bf4e1008daf506853cde7c09a71e86863
dmitrii_zaitsev <zaycevdi@gmail.com>
2021-10-27 22:06:06 +0300

    git init

---
+++ src/services/MarvelService.js
@@ -0,0 +1,21 @@
+export class MarvelService {
+  _apiBase = 'https://gateway.marvel.com:443/v1/public/';
+  _apiKey = 'apikey=d3703dc8816c5b16d676eb3034770828';
+
+  getResource = async (url) => {
+    let res = await fetch(url);
+
+    if (!res.ok) {
+      throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+    }
+
+    return await res.json();
+  }
+
+  getAllCharacters = () => {
+    return this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+  }
+  getCharacter = (id) => {
+    return this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
+  }
+}

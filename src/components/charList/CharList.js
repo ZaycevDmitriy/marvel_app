6fdf149f9f0d75ee36e34410e8eeb3858cd32b51
dmitrii_zaitsev <zaycevdi@gmail.com>
2021-11-07 21:51:18 +0300

    Переписал компонент CharList из классового в функциональный с применением хуков.

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -1,119 +1,99 @@
 import './charList.scss';
-import {Component} from "react";
+import {useEffect, useState} from "react";
 import {MarvelService} from "../../services/MarvelService";
-import {CharItem} from "./charItem/CharItem";
+import CharItem from "./charItem/CharItem";
 import {ErrorMessage} from "../ErrorMessage/ErrorMessage";
 import {Spiner} from "../spiner/Spiner";
 
-class CharList extends Component {
-  state = {
-    chars: new Array(9).fill(undefined),
-    firstBoot: true,  // первая загрузка
-    loaded: true, // идет загрузка персонажей, крутится спинер
-    error: false,
-    offset: 210,
-    loadingBtn: false,
-    ending: false,
+const CharList = (props) => {
+  const [chars, setChars] = useState(new Array(9).fill(undefined));
+  const [firstBoot, setFirstBoot] = useState(true);
+  const [loaded, setLoaded] = useState(true);
+  const [error, setError] = useState(false);
+  const [offset, setOffset] = useState(210);
+  const [loadingBtn, setLoadingBtn] = useState(false);
+  const [ending, setEnding] = useState(false);
+
+  const marvelService = new MarvelService();
+
+  const onError = () => {
+    setLoaded(false);
+    setError(true);
   }
 
-  marvelService = new MarvelService();
-
-  onError = () => {
-    this.setState({loaded: false, error: true});
-    clearInterval(this.idInterval);
-    this.idInterval = setInterval(() => (this.updateChars), 3000);
+  const updateChars = () => {
+    onLoadingChars();
   }
 
-  // onSelectChange = (id) => {
-  //   this.setState(({chars}) => {
-  //     return {chars: chars.map(char => {
-  //       if(char.id === id) {
-  //         return {...char, select: true};
-  //       } else {
-  //         return {...char, select: false};
-  //       }
-  //     })}
-  //   })
-  // }
-
-  updateChars = () => {
-    this.onLoadingChars();
-  }
-
-  onLoadingChars = (offset) => {
+  const onLoadingChars = (offset) => {
     if (offset > 1556) {
-      this.setState({ending: true})
+      setEnding(true);
     }
-    this.marvelService
+    marvelService
       .getAllCharacters(offset)
-      .then(this.onLoadedChars)
-      .catch(this.onError);
+      .then(onLoadedChars)
+      .catch(onError);
   }
 
-  onChangeLoadingBtn = () => {
-    this.setState({loadingBtn: true})
+  const onChangeLoadingBtn = () => {
+    setLoadingBtn(true);
   }
 
-  onLoadedChars = (newChars) => {
+  const onLoadedChars = (newChars) => {
     // при первой загрузке переписываем array chars
-    if(this.state.firstBoot) {
-      this.setState({
-        chars: newChars,
-        loaded: false,
-        error: false,
-        firstBoot: false,
-      });
+    if(firstBoot) {
+      setChars(newChars);
+      setLoaded(false);
+      setError(false);
+      setFirstBoot(false);
     } else {
-      this.setState(({chars, offset}) => ({
-        chars: [...chars, ...newChars],
-        offset: offset + 9,
-        loadingBtn: false,
-      }));
+      setChars(chars => [...chars, ...newChars]);
+      setOffset(offset => offset + 9);
+      setLoaded(false);
+      setLoadingBtn(false);
     }
   }
 
-  componentDidMount() {
-    this.updateChars();
-  }
+  useEffect(() => {
+    console.log('mount');
+    updateChars();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
-  render() {
-    const {chars, loaded, error, loadingBtn, ending} = this.state;
+  const charItem = chars.map((char, index) => {
+    if(char) {
+      return (
+        <CharItem name={char.name}
+                  thumbnail={char.thumbnail}
+                  key={char.id}
+                  id={char.id}
+                  onChangeId={props.onChangeId}
+        />
+      )
+    } else {
+      return (
+        <ViewAlt loaded={loaded} error={error} key={index}/>
+      )
+    }
+  });
 
-    const charItem = chars.map((char, index) => {
-      if(char) {
-        return (
-          <CharItem name={char.name}
-                    thumbnail={char.thumbnail}
-                    key={char.id}
-                    id={char.id}
-                    onChangeId={this.props.onChangeId}
-                    // onSelectChange={this.onSelectChange}
-          />
-        )
-      } else {
-        return (
-          <ViewAlt loaded={loaded} error={error} key={index}/>
-        )
-      }
-    });
+  return (
+    <div className="char__list">
+      <ul className="char__grid">
+        {charItem}
+      </ul>
+      <button className="button button__main button__long"
+      onClick={() => {
+        onLoadingChars(offset + 9);
+        onChangeLoadingBtn();
+      }}
+      disabled={loadingBtn}
+      style={ending ? {display: 'none'} : {display: 'block'}}>
+        <div className="inner">{loadingBtn ? 'loading ...' : 'load more'}</div>
+      </button>
+    </div>
+  )
 
-    return (
-      <div className="char__list">
-        <ul className="char__grid">
-          {charItem}
-        </ul>
-        <button className="button button__main button__long"
-        onClick={() => {
-          this.onLoadingChars(this.state.offset + 9);
-          this.onChangeLoadingBtn();
-        }}
-        disabled={loadingBtn}
-        style={ending ? {display: 'none'} : {display: 'block'}}>
-          <div className="inner">{loadingBtn ? 'loading ...' : 'load more'}</div>
-        </button>
-      </div>
-    )
-  }
 }
 
 const ViewAlt = ({error, loaded}) => {
